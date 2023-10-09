38d20e0668c782a235556d2b5d327e31c6277617
LUCY <wangecikariu.luc@gmail.com>
2023-10-09 14:58:26 +0300

    Finished Lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
-# your code goes here!
+def countdown(count_down):
+    count_down = 0
+    while count_down < 10:
+        print(f"{count_down} SECOND(S)!")
+        count_down -=1
+    print("HAPPY NEW YEAR!")
+
+import time
+
+def countdown_with_sleep(countdown):
+    countdown = 0
+    while countdown < 10:
+        print(f"{countdown} SECOND(S)!")
+        countdown -= 1
+    print("HAPPY NEW YEAR!")    
